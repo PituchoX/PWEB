e61d4e14fce1f1356ed008668fc6509201a37ed3
João Oliveira <joaopitucho15@gmail.com>
2026-01-04 01:55:58 +0000

    atualizado

--- GestaoLoja/GestaoLoja/obj/Debug/net8.0/GestaoLoja.AssemblyInfo.cs
+++ GestaoLoja/GestaoLoja/obj/Debug/net8.0/GestaoLoja.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoLoja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+924d5c3f37d17052ecb2f23b0e9987ae48de38f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474acc1a958c6e4e976e33b2c1300eafb558f47d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoLoja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoLoja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
